f90ee85a49d121169354869040330e0a100085c4
heyarne <arne@schlueter.is>
2022-12-13 14:54:47 +0100

    Add usage examples

--- README.md
+++ README.md
@@ -9,5 +9,11 @@ Usage: check-katex [-u|--start-url ...] [-h|--help]
   -u, --start-url Starting point of the crawler (default: https://ems.press/journals)
   -h, --help      prints help information
 
-Will only check URLs deeper than the given start URL.
+Will only check URLs deeper than the given start URL. All errors are printed
+stderr, verbose request information is printed to stdout.
+
+Examples:
+
+   go run main.go > /dev/null # only print errors
+   go run main.go > /dev/null  2>&1 | tee errors.log # save all errors to a file
 ```
--- main.go
+++ main.go
@@ -16,7 +16,13 @@ func main() {
   -u, --start-url Starting point of the crawler (default: https://ems.press/journals)
   -h, --help      prints help information
 
-Will only check URLs deeper than the given start URL.
+Will only check URLs deeper than the given start URL. All errors are printed
+stderr, verbose request information is printed to stdout.
+
+Examples:
+
+   go run main.go > /dev/null # only print errors
+   go run main.go > /dev/null  2>&1 | tee errors.log # save all errors to a file
 `
 	startUrl := "https://ems.press/journals"
 	flag.StringVar(&startUrl, "start-url", startUrl, "Start point of the crawler")
